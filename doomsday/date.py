05f7c5d3cb87e88271104222641ef9ba4ad9cad9
MaximeVerger <vergermaxime.sio@gmail.com>
2021-10-19 22:23:59 +0200

    corrections in date.py

--- doomsday/date.py
+++ doomsday/date.py
@@ -1,44 +1,44 @@
 def is_valid_date(target_date: str) -> bool:
-    return False if type(target_date) != str else is_valid_year(target_date) and is_valid_month(target_date) and is_valid_day(target_date) and has_right_number_of_day(target_date)
+    if type(target_date) == str:
+        year: str = target_date[0:4]
+        month: str = target_date[5:7]
+        day: str = target_date[8:]
+        return is_valid_year(year) and is_valid_month(month) and is_valid_day(day) and has_right_number_of_day(year, month, day) and is_valid_format(target_date)
+    else:
+        return False
 
-def is_valid_year(target_date: str) -> bool:
-    year: str = target_date[0:4]
-    for char in year:
-        if char.isnumeric() == False:
-            print("Erreur : Un ou plusieurs caractères ne sont pas des chiffres dans l'année")
-            return False
+def is_valid_year(year: str) -> bool:
+    if year.isnumeric() == False:
+        print("Erreur : Un ou plusieurs caractères ne sont pas des chiffres dans l'année")
+        return False
     return int(year) > 1583
 
-def is_valid_month(target_date: str) -> bool:
-    month: str = target_date[5:7]
-    for char in target_date[5:7]:
-        if char.isnumeric() == False:
-            print("Erreur : Un ou plusieurs caractères ne sont pas des chiffres dans le mois")
-            return False
+def is_valid_month(month: str) -> bool:
+    if month.isnumeric() == False:
+        print("Erreur : Un ou plusieurs caractères ne sont pas des chiffres dans le mois")
+        return False
     return int(month) <= 12
     
-def is_valid_day(target_date: str) -> bool:
-    day: str = target_date[8:]
-    for char in day:
-        if char.isnumeric() == False:
-            print("Erreur : Un ou plusieurs caractères ne sont pas des chiffres dans le jour")
-            return False
-    return int(day) <= 31 if day[0] != 0 else True
+def is_valid_day(day: str) -> bool:
+    if day.isnumeric() == False:
+        print("Erreur : Un ou plusieurs caractères ne sont pas des chiffres dans le jour")
+        return False
+    return int(day) <= 31 and int(day) > 0
 
 def is_valid_format(target_date: str) -> bool:
-    print("Erreur : La date n'est pas au bon format")
-    return target_date[4] == "-" and target_date[7] == "-"
+    if target_date[4] != "-" or target_date[7] != "-":
+        print("Erreur : La date n'est pas au bon format")
+        return False
+    else:
+        return target_date[4] == "-" and target_date[7] == "-"
 
 def is_leap_year(year: str) -> bool:
     year_numeric: int = int(year)
     return (year_numeric % 400 == 0) or (year_numeric % 4 == 0 and year_numeric % 100 != 0)
 
-def has_right_number_of_day(target_date: str) -> bool:
-    if is_valid_day(target_date) == False or is_valid_month(target_date) == False:
+def has_right_number_of_day(year: str, month: str, day: str) -> bool:
+    if not is_valid_day(day) or not is_valid_month(month):
         return False
-    day: str = target_date[8:]
-    month: str = target_date[5:7]
-    year: str = target_date[0:4]
     if month == "02" and is_leap_year(year):
         return int(day) <= 29 if day[0] != 0 else True
     elif month == "02" and is_leap_year(year) == False:
