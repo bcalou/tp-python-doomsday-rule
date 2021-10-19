17084acf359259e49245ee64c27df78696c5fcc3
Axelle HUMBERT <humbertaxelle40@gmail.com>
2021-10-19 21:25:49 +0200

    Add of docstrings into date.py

--- doomsday/date.py
+++ doomsday/date.py
@@ -1,5 +1,11 @@
 import re
 
+"""
+Return boolean.
+Check the validity of the date.
+Keyword arguments:
+input_date -- Date input by the user
+"""
 
 def is_valid_date(input_date: str) -> bool:
 
@@ -13,10 +19,16 @@ def is_valid_date(input_date: str) -> bool:
         return False
     return True
 
-
+"""
+Return boolean.
+Check the validity of the date format.
+Keyword arguments:
+target -- date input by the user.
+"""
 def is_valid_format(target) -> bool:
 
     is_valid_input_type: bool = False if type(target) != str else True
+    """A regular  expression for the format (YYYY-MM-dd)"""
     format = re.compile("(1[5-9][0-9][0-9]|2[0-9][0-9][0-9])-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[0-1])")
 
     if not is_valid_input_type:
@@ -26,7 +38,12 @@ def is_valid_format(target) -> bool:
 
     return True if is_valid_input_type and format.match(target) else False
 
-
+"""
+Return boolean.
+Check the number of days in the month.
+Keyword arguments:
+target -- date input by the user.
+"""
 def is_valid_count_day(target) -> bool:
 
     year, month, day = get_split_date(target)
@@ -46,12 +63,22 @@ def is_valid_count_day(target) -> bool:
 
     return is_valid_day_count
 
-
+"""
+Return boolean.
+Checked the date is later than 1583
+Keyword arguments:
+target -- date input by the user.
+"""
 def is_valid_year(target) -> bool:
     year: int = get_split_date(target)[0]
     return year >= 1583
 
-
+"""
+Return tuple
+Split the data into three variables.
+Keyword arguments:
+target -- date input by the user.
+"""
 def get_split_date(target) -> tuple:
     split_date: list[str] = target.split('-')
 
@@ -61,6 +88,12 @@ def get_split_date(target) -> tuple:
 
     return year, month, day
 
-
-def is_leap_year(target):
+"""
+Return boolean
+Checked if the year is a leap year
+Keyword arguments:
+target -- the year of data input by the user.
+"""
+def is_leap_year(target) -> bool:
+    """If is a leap year is a multiple of 4 but not of 100 and is a multiple of 400"""
     return (target % 400 == 0) or (target % 4 == 0 and target % 100 != 0)
