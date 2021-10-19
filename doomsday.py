ee13b4cb20eb6fee2acbae6193edd22513b469c6
Lucas JAOUEN <lucas77foot@gmail.com>
2021-10-19 11:51:49 +0200

    tp-doomsday-rule-Lucas_JAOUEN

---
+++ doomsday.py
@@ -0,0 +1,19 @@
+from doomsday.date import is_valid_date
+from doomsday.algorithm import get_day_for_date
+from doomsday.algorithm import getDoomsday
+
+def ask_date(date: str) -> str:
+    
+    while(True):
+        date = input("Entrer une date au format: YYYY-MM-dd")
+        if not is_valid_date(date):
+            print("Vous avez commis une erreur de saisie vérifiez bien que le format de la date soit YYYY-MM-dd")
+        
+        else :
+            print("Bien reçu")
+
+        break
+
+    return date
+
+  
--- doomsday/algorithm.py
+++ doomsday/algorithm.py
@@ -1,2 +1,16 @@
+from doomsday.date import is_leap
+
+
 def get_day_for_date(date: str) -> str:
     return "Monday"
+
+
+def getDoomsday(year: int, month:int) -> int:
+    return 1
+
+def getYearsBissex(day : int) -> str:
+    return 1
+
+def getDaysCarac(day: int) -> str:
+    return ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"]
+
