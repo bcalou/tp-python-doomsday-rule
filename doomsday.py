31ddd8ca42ef931452ca802bdac3516d14162255
Tryptoo <nico.perot@hotmail.fr>
2021-10-21 11:09:40 +0200

    Quelques changements

--- doomsday.py
+++ doomsday.py
@@ -1,11 +1,9 @@
-import doomsday
 from doomsday import date
 
 input_date:str = ""
-is_valide_date:bool = False
-while(not input_date.isdigit() and not is_valide_date):
+is_valid_date:bool = False
+while(not is_valid_date):
     is_a_wrong_date=True
     input_date = input("Rentrer une date au format YYYY-MM-dd (mettre les '-' sans espace entre les champs) : \n")
-    if(date.is_valid_date(input_date)):
+    if(date.is_valid_date(input_date) and input_date.isdigit()):
         is_valide_date = True
-1
--- doomsday/date.py
+++ doomsday/date.py
@@ -1,14 +1,14 @@
 from doomsday import algorithm
 
 def is_valid_date(date: str) -> bool:
+    
     if(str(date).count("-") <2):
         print("Il n'y a pas ou manque des '-' dans la date")
         return False
-    dates:list[str] = []
     dates = date.split("-")
-    return first_validation_input_date(dates)
+    return format_validation_input_date(dates)
 
-def first_validation_input_date(dates:list[str])-> bool:
+def format_validation_input_date(dates:list[str])-> bool:
     is_wrong_date:bool = False
     if(len(dates[0])!= 4):
         is_wrong_date=True
@@ -21,21 +21,21 @@ def first_validation_input_date(dates:list[str])-> bool:
         print("Jour invalide, mettez un jour existant au format dd")
     if(is_wrong_date):
         return False
-    return second_validation_year(dates)
+    return format_validation_year(dates)
 
-def second_validation_year(dates:list[str])->bool :
+def format_validation_year(dates:list[str])->bool :
     if(int(dates[0]) > 1583):
-        return third_validation_month(dates)
+        return format_validation_month(dates)
     print("Année invalide, mettez une année postérieur à 1583")
     return False
 
-def third_validation_month(dates:list[str])->bool :
+def format_validation_month(dates:list[str])->bool :
     if(int(dates[1]) >= 1 and int(dates[1]) <=12) :
-        return fourth_validation_day(dates)
+        return format_validation_day(dates)
     print("Mois invalide, mettez un mois entre 01 et 12")
     return False
 
-def fourth_validation_day(dates:list[str])->bool :
+def format_validation_day(dates:list[str])->bool :
     MONTHS: list[list[int]] = [[1,3,5,7,8,10,12],[4,5,9,11]]
     if(MONTHS[0].count(int(dates[1])) == 1):
         return long_months_validation_day(dates)
