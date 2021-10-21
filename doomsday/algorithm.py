faf9cce6aea88ed366ec7fa104861a5730fed169
Johan <johan.guerrero394@gmail.com>
2021-10-21 11:25:37 +0200

    Refactoring comments and add basics optimisation (variables range)

--- doomsday/algorithm.py
+++ doomsday/algorithm.py
@@ -1,43 +1,42 @@
 from doomsday.date import *
 
-days: list[str] = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-    ]
-common_year_pivots: list[int] = [3, 0, 0, 4, 9, 6, 11, 8, 5, 10, 7, 12]
-leap_year_pivots: list[int] = [4, 1, 0, 4, 9, 6, 11, 8, 5, 10, 7, 12]
+
+def get_year_pivots(month: int, year: int) -> int:
+    """Return the date of the pivot day in the selected month and year"""
+    year_pivots: list[int] = [
+        (3, 4)[is_leap_year(year)],
+        (0, 1)[is_leap_year(year)],
+        0, 4, 9, 6, 11, 8, 5, 10, 7, 12]
+    return year_pivots[month - 1]
 
 
-# The century marker is an empirical value
-# based on the hundred digit over a 4 century loop
 def get_century_marker(year: int) -> int:
+    """The century marker is an empirical value
+    based on the hundred digit over a 4 century loop"""
     y: int = (year % 400) // 100
     return [2, 0, 5, 3][y]
 
 
-# "Odd + 11" methode
-def compute(year: int) -> int:
+def compute_date(year: int) -> int:
+    """Compute the year to get a reference value based
+    on the \"Odd + 11\" methode"""
     y_last_digit: int = year % 100
-    y_last_digit = y_last_digit if y_last_digit % 2 == 0 else y_last_digit + 11
+    y_last_digit = (y_last_digit + 11, y_last_digit)[y_last_digit % 2 == 0]
     y_last_digit //= 2
-    y_last_digit = y_last_digit if y_last_digit % 2 == 0 else y_last_digit + 11
+    y_last_digit = (y_last_digit + 11, y_last_digit)[y_last_digit % 2 == 0]
     y_last_digit = 7 - (y_last_digit % 7)
     return (y_last_digit + get_century_marker(year)) % 7
 
 
-# return the day of the date passed in argument
-# using John Conway's Doomsday Methode
 def get_day_for_date(date: str) -> str:
-    year, month, day = convert_date(date)
-    anchor_day: int = compute(year)
-    if is_leap_year(year):
-        month_pivots_day = leap_year_pivots[month - 1]
-    else:
-        month_pivots_day = common_year_pivots[month - 1]
+    """Return the day of the date passed in argument
+    using John Conway's Doomsday Methode"""
+    days: list[str] = [
+        "Sunday", "Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday"
+        ]
+    date_as_int : list[int] = convert_date(date)
+    anchor_day: int = compute_date(date_as_int[0])
+    month_pivots_day = get_year_pivots(date_as_int[1], date_as_int[0])
     # shift from the anchor day by the difference between day and pivot
-    return days[(anchor_day + (day - month_pivots_day)) % 7]
+    return days[(anchor_day + (date_as_int[2] - month_pivots_day)) % 7]
--- doomsday/date.py
+++ doomsday/date.py
@@ -1,13 +1,13 @@
 import re
 
 
-# Return True if the year passed in argument is a leap year
 def is_leap_year(y: int) -> bool:
+    """Return True if the year passed in argument is a leap year"""
     return(y % 4 == 0 and y % 100 != 0) or y % 400 == 0
 
 
-# Return True if date is at format -> YYYY-MM-dd
 def is_valid_date(date: str) -> bool:
+    """Return True if date is at format -> YYYY-MM-dd"""
     month_max_lenght = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     m = re.match(r"(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})", str(date))
     if m is None:
@@ -21,7 +21,8 @@ def is_valid_date(date: str) -> bool:
     return True
 
 
-# convert date from one chain to 3 ints (year, month, day)
-def convert_date(date: str) -> int:
+def convert_date(date: str) -> list[int]:
+    """ Convert date from one chain to 3 ints
+    return List[int] as [year, month, day]"""
     splited_date: list[str] = date.split('-')
-    return int(splited_date[0]), int(splited_date[1]), int(splited_date[2])
+    return [int(splited_date[0]), int(splited_date[1]), int(splited_date[2])]
