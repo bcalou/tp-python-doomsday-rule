9296fabd3c26f1f4ad5d334a589ee2ceb5b4a676
Hippolyte Guillemeteau <HippolyteG16@gmail.com>
2021-10-21 11:14:43 +0200

    Hippolyte Guillemeteau Fix After Review

--- doomsday/algorithm.py
+++ doomsday/algorithm.py
@@ -1,83 +1,44 @@
-def bixestil(year):
-    if(year % 4 == 0):
-        if(year % 100 == 0):
-            if(year % 400 == 0):
-                return True
-            else:
-                return False
-        else:
-            return True
-    else:
-        return False
+def leap_year(year: int) -> bool:
+    return (year % 4 == 0 and year % 100 != 0 or year % 400 == 0)
 
 
-def months_finder(month, year):
-    switcher = {
-        1: 10,
-        2: 21,
-        3: 0,
-        4: 4,
-        5: 9,
-        6: 6,
-        7: 11,
-        8: 8,
-        9: 5,
-        10: 10,
-        11: 7,
-        12: 12
-    }
-    number = int(switcher.get(month, "Invalid months"))
-    if((month == 1 or month == 2) and bixestil(year)):
-        return number+1
-    else:
-        return number
+def months_finder(month: int, year: int):
+    tab_months = [10, 21, 0, 4, 9, 6, 11, 8, 5, 10, 7, 12]
+    number = tab_months[month-1]
+    return number+1 if((month == 1 or month == 2) and leap_year(year)) else number
 
 
-def calculation_year(year):
+def calculation_anchor_day(year: int) -> int:
     list_year = str(year)
     day = list_year[2]+list_year[3]
     day = int(day)
-    if(day % 2 == 0):
-        number = day/2
-        if(number % 2 == 0):
-            return -number % 7
-        else:
-            return -(number+11) % 7
-    else:
-        number = (day + 11)/2
-        if(number % 2 == 0):
-            return -number % 7
-        else:
-            return -(number+11) % 7
+    if(not day % 2 == 0):
+        day += 11
+    day = day/2
+    if(not day % 2 == 0):
+        day += 11
+    return int(-day % 7)
 
 
-def year_century(year_century):
-    modulo = year_century % 400
-    if(modulo >= 0 and modulo < 100):
+def year_century(year_century: int) -> int:
+    modulo = int(year_century / 100 % 4)
+    if(modulo == 0):
         return 2
-    elif(modulo >= 100 and modulo < 200):
+    elif(modulo == 1):
         return 0
-    elif(modulo >= 200 and modulo < 300):
+    elif(modulo == 2):
         return 5
     return 3
 
 
-def get_day_for_date(date: str):
+def get_day_for_date(date: str) -> str:
     list_date = date.split('-')
     year = int(list_date[0])
     month = int(list_date[1])
     day = int(list_date[2])
-    days = {
-        0: "Sunday",
-        1: "Monday",
-        2: "Tuesday",
-        3: "Wednesday",
-        4: "Thursday",
-        5: "Friday",
-        6: "Saturday"
-    }
-    day_key = int((calculation_year(year)+year_century(year)))
+    days = ["Sunday", "Monday", "Tuesday",
+            "Wednesday", "Thursday", "Friday", "Saturday"]
+    day_key = int((calculation_anchor_day(year)+year_century(year)))
     day_pivot = (months_finder(month, year))
     diff_day = (day_pivot - day)
-
     return days[(day_key-diff_day) % 7]
--- doomsday/date.py
+++ doomsday/date.py
@@ -1,49 +1,41 @@
-def bixestil(year):
-    if(year % 4 == 0):
-        if(year % 100 == 0):
-            if(year % 400 == 0):
-                return True
-            else:
-                return False
-        else:
-            return True
-    else:
-        return False
+from doomsday.algorithm import leap_year
 
 
-def is_valid_date(date: str) -> bool:
+def is_valide_input(date: str) -> bool:
     if(not(isinstance(date, str))):
         print("Error Format String Request")
         return False
-    count_dash = 0
-    for char in date:
-        if(char == "-"):
-            count_dash = count_dash + 1
-    if(count_dash != 2):
-        print("Error Format Missing Dash")
-        return False
     list_date = date.split('-')
     if(len(list_date) != 3):
+        print("Error Format Date YYYY-MM-dd")
         return False
-    year = int(list_date[0])
-    month = int(list_date[1])
-    day = int(list_date[2])
-    day_february = 28
-    if(bixestil(year)):
-        day_february = day_february+1
-    if (month < 0 or month > 12) or (day < 1 or day > 31) or year < 1583:
+    return True
+
+
+def is_valid_date(date: str) -> bool:
+    if(is_valide_input(date)):
+        list_date = date.split('-')
+        year = int(list_date[0])
+        month = int(list_date[1])
+        day = int(list_date[2])
+        day_february = 28
+        if(leap_year(year)):
+            day_february = day_february+1
         if(month < 0 or month > 12):
             print("Error Date Month")
+            return False
         elif((day < 1 or day > 31)):
             print("Error Date Day")
-        else:
+            return False
+        elif(year < 1583):
             print("Error Date year < 1583")
-        return False
-    elif((month == 4 or month == 6 or month == 8 or month == 10 or month == 12) and day > 30):
-        print("Error Date Day for Peer Months")
-        return False
-    elif(month == 2 and day > day_february):
-        print("Error Date Day for February")
-        return False
-    else:
-        return True
+            return False
+        elif((month == 4 or month == 6 or month == 8 or month == 10 or month == 12) and day > 30):
+            print("Error Date Day for Even Months")
+            return False
+        elif(month == 2 and day > day_february):
+            print("Error Date Day for February")
+            return False
+        else:
+            return True
+    return False
