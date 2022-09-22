b870d0b8c5297fa5f3259d722771148e7d8bb346
bcalou <bastien.calou@gmail.com>
2022-09-22 17:12:17 +0200

    update tests

--- test-date.py
+++ test-date.py
@@ -12,6 +12,9 @@ elif not is_valid_date('2000-02-29'):
 elif not is_valid_date('2148-01-31'):
     print('\033[91m❌ 2148-01-31 should be valid')
 
+elif not is_valid_date('2148-1-2'):
+    print('\033[91m❌ 2148-1-2 should be valid')
+
 elif is_valid_date('2021-20-00'):
     print('\033[91m❌ 2021-20-00 should not be valid')
 
@@ -35,6 +38,12 @@ elif is_valid_date('01-01-'):
 
 elif is_valid_date('-01-2020'):
     print('\033[91m❌ -01-2020 should not be valid')
+
+elif is_valid_date('10-01-2020-02'):
+    print('\033[91m❌ 10-01-2020-02 should not be valid')
+
+elif is_valid_date('10-01'):
+    print('\033[91m❌ 10-01 should not be valid')
 
 else:
     print('\033[92m✓ OK')
