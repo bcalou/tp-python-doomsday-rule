6348459504331ee5acbce100f17293f587918c18
Antoine <antoine.martin885@gmail.com>
2022-09-23 16:36:30 +0200

    Initial commit

--- doomsday.py
+++ doomsday.py
@@ -2,3 +2,20 @@ from doomsday.date import is_valid_date
 from doomsday.algorithm import get_day_for_date
 
 # Commencez ici !
+
+# Demander date au format YYYY-MM-dd
+
+def input_date() -> None:
+    """Asks for a date and check if its valid"""
+
+
+
+# Dans doomsday/date:
+# Vérifier que la date est bien au format YYYY-MM-dd
+
+# La date existe et est supérieure à 1953
+
+# Day = n° du jour (entre 1 et 31)
+# Week_day = jour de la semaine (lundi, ...)
+
+# Retour d'erreurs sur la date
