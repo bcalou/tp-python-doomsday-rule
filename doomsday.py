5bfd31ab1c966a3d5fe7c8fc1d7cb19b727b1d15
Jame <jame.flochlecarour@gmail.com>
2022-09-23 16:57:09 +0200

    Added date input structure in doomsday.py

--- doomsday.py
+++ doomsday.py
@@ -7,5 +7,13 @@ from doomsday.algorithm import get_day_for_date
 print("Hello Doomsday !")
 print("Bienvenue sur ’algorithme du jour du jugement dernier !\n")
 
-# Instructions
-date_input: str = input("Veuillez indiquer la date au format YYYY-MM-dd :\n\n")
+
+is_input_date_valid: bool = False
+date_input: str = ""
+
+# Re ask date input until a valid date is given 
+while not is_input_date_valid:
+
+    # Instructions
+    date_input = input("Veuillez indiquer la date au format YYYY-MM-dd :\n\n")
+    is_input_date_valid = is_valid_date(date_input)
