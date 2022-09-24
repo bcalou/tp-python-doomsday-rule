93b1c1305c9cac2802f1306dd226cbdcb1b475e4
Colin BERNARD <colin.bernard@ymail.com>
2022-09-24 19:44:33 +0200

    date.py reorganized with subfunctions

--- doomsday.py
+++ doomsday.py
@@ -2,5 +2,3 @@ from doomsday.date import is_valid_date
 from doomsday.algorithm import get_day_for_date
 
 # Commencez ici !
-
-coucou: str = "TESTuhgtuhuthguthguthguthgtuhgtuhg"
--- doomsday/date.py
+++ doomsday/date.py
@@ -1,71 +1,149 @@
+str_date_list: list[str] = []
+int_date_list: list[int] = []
+year: int = 0
+month: int = 0
+day: int = 0
+
+
+
 def is_valid_date(date: str) -> bool:
     """Checking if entered string format is correct and if the date exists"""
 
-    dash_quantity: int = 0
-
-    for character in date:
-        if character == "-":
-            dash_quantity += 1
+    if not is_string_format_correct(date):
+        return False
+    else:
+        create_date_list()
+        store_values_from_date_list_in_variables()
     
-    if dash_quantity != 2:
+    if not does_year_exists(year):
+        return False
+    
+    if not does_month_exists(month):
+        return False
+    
+    if not does_day_exists(day):
+        return False
+    
+    reset_global_variables()
+    return True
+
+
+
+def is_string_format_correct(input: str) -> bool:
+    dash_quantity_in_input_string: int = 0
+
+    for character in input:
+        if character == "-":
+            dash_quantity_in_input_string += 1
+    
+    if dash_quantity_in_input_string != 2:
         print("Please format date with dashes")
         return False
     
-    date_list: list[str] = date.split('-')
-    year: str = date_list[0]
-    month: str = date_list[1]
-    day: str = date_list[2]
+    global str_date_list
+    str_date_list = input.split('-')
+    local_year: str = str_date_list[0]
+    local_month: str = str_date_list[1]
+    local_day: str = str_date_list[2]
 
-    if not year.isdecimal():
+    if not local_year.isdecimal():
         print("Please enter a correct year")
         return False
     
-    if not month.isdecimal():
+    if not local_month.isdecimal():
         print("Please enter a correct month")
         return False
     
-    if not int(year) >= 1583:
-        print("Please enter a year after 1583")
-        return False
-    
-    if not 0 < int(month) <= 12:
-        print("This month doesn't exist")
-        return False
-    
-    months_with_30_days: list[int] = [4, 6, 9, 11]
-    months_with_31_days: list[int] = [1, 3, 5, 7, 8, 10, 12]
-
-    if not day.isdecimal():
+    if not local_day.isdecimal():
         print("Please input a correct day")
         return False
     
+    return True
+
+
+
+def create_date_list():
+    global int_date_list
+
+    for index in range(len(str_date_list)):
+        int_date_list.append(int(str_date_list[index]))
+
+
+
+def store_values_from_date_list_in_variables():
+    global year, month, day
+    year = int_date_list[0]
+    month = int_date_list[1]
+    day = int_date_list[2]
+
+
+
+def does_year_exists(given_year: int) -> bool:
+    if given_year >= 1583:
+        return True
+    else:
+        print("Please enter a year after 1583")
+        return False
+
+
+
+def does_month_exists(given_month: int) -> bool:
+    if 0 < given_month <= 12:
+        return True
+    else:
+        print("This month doesn't exist")
+        return False
+
+
+
+def does_day_exists(given_day: int) -> bool:
+    months_with_30_days: list[int] = [4, 6, 9, 11]
+    months_with_31_days: list[int] = [1, 3, 5, 7, 8, 10, 12]
+    
     if (
-        (int(month) in months_with_30_days) and (not 0 < int(day) <= 30) or
-        (int(month) in months_with_31_days) and (not 0 < int(day) <= 31)
+        (month in months_with_30_days) and (not 0 < given_day <= 30) or
+        (month in months_with_31_days) and (not 0 < given_day <= 31)
     ):
         print("This day doesn't exist in this month")
         return False
     
     if (
-        int(month) == 2 and
-        ((is_leap_year(int(year)) and not 0 < int(day) <= 29) or
-        (not is_leap_year(int(year)) and not 0 < int(day) <= 28))
+        month == 2 and
+        ((is_leap_year(year) and not 0 < given_day <= 29) or
+        (not is_leap_year(year) and not 0 < given_day <= 28))
     ):
         print("This day doesn't exist in this month (please check if year is leap)")
         return False
-
+    
     return True
 
 
-def is_leap_year(year: int) -> bool:
-    """Check is year is leap or not"""
-    return (year % 4 == 0 and not year % 100 == 0) or (year % 400 == 0)
+
+def is_leap_year(given_year: int) -> bool:
+    """Check is year is leap or not.
+    See https://www.mathsisfun.com/leap-years.html for calculation rules.
+    """
+    return (given_year % 4 == 0 and not given_year % 100 == 0) or (given_year % 400 == 0)
+
+
+
+def reset_global_variables():
+    global str_date_list, int_date_list
+    global year, month, day
+    str_date_list = []
+    int_date_list = []
+    year = 0
+    month = 0
+    day = 0
+
+
+
+
 
 """
 date_to_test: str = "2021-10-10"
 print(f"Is {date_to_test} a valid date ?", is_valid_date(date_to_test))
 """
-
 
 
 
@@ -75,8 +153,17 @@ print(f"Is {date_to_test} a valid date ?", is_valid_date(date_to_test))
 QUESTIONS
 ---------
 
-Est-ce que fonction trop longue ? Pertinent de sous-découper en mini fonctions ?
-(is_string_format_correct(), does_year_exist(), does_month_exist(), does_day_exist())
+Noms des variables ok avec préfixe local_ dans is_string_format_correct() ?
 
 Convention respectée pour les if avec des retours à la ligne ?
+
+2 lignes vides entre chaque def de fonction ? Actuellement 3
+
+
+-----
+TO DO
+-----
+Modifs en fonction des réponses aux questions ci-dessus
+
+Clean commentaires en bas de fichier
 """
