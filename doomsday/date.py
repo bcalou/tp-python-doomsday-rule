7fd227a9b5834d228ff7862c6c7b96621f0cccb6
82L <16747176+82L@users.noreply.github.com>
2022-09-24 23:50:07 +0200

    Date validity test algorithm

--- doomsday/date.py
+++ doomsday/date.py
@@ -1,2 +1,89 @@
+GREGORIAN_CALENDAR_START: int = 1583
+FEBRUARY_NUMBER: int = 2
+MAX_DATE_MONTH: int = 31
+MIN_LENGTH_DATE_STRING: int = 8
+DATE_ELEMENT_NUMBER: int = 3
+
 def is_valid_date(date: str) -> bool:
-    return False
+    """Look if the date does not violate the terms for conway algorithm"""
+
+    if len(date) < MIN_LENGTH_DATE_STRING :
+        print("Date is not long enough to be valid")
+        return False
+    
+    date_as_list: list[str] = date.split("-")
+
+    if len(date_as_list) != DATE_ELEMENT_NUMBER:
+        print("Too much or too less date elements")
+        return False
+    
+    year: str = date_as_list[0]
+    month: str = date_as_list[1]
+    day: str = date_as_list[2]
+
+    if not is_valid_year(year):
+        return False
+    
+    if not is_valid_month_and_day(month, day, is_year_bissextile(int(year))):
+        return False
+    
+    return True
+    
+
+def is_valid_year(year: str) -> bool:
+    """Check if year is a number above start of gregorian calendar"""
+
+    if not year.isdecimal():
+        print("Year must be a number")
+        return False
+    
+    if int(year) < GREGORIAN_CALENDAR_START: 
+        print("Year must be after 1582")
+        return False
+    
+    return True
+
+
+def is_year_bissextile(year: int) -> bool:
+    """Check if a year is multiple of four, and not multiple of 100 but multiple of 400"""
+    return (year % 4 == 0 and ( year % 100 != 0 or year % 400 == 0))
+
+
+def is_valid_month_and_day(month: str, day: str, is_year_bissextile: bool) -> bool:
+    
+    if not (is_a_number(month, "Month") and is_a_number(day, "Day")):
+        return False
+    
+    int_day:int = int(day)
+    int_month:int = int(month)
+
+    if 1 > int_month or int_month > 12:
+        print("Month must be between 1 and 12")
+        return False
+
+    if 1 > int_day:
+        print("Day must be positive and above 1")
+        return False
+
+    if (int_month == FEBRUARY_NUMBER and
+        ((is_year_bissextile and int_day > 29) or 
+        (not is_year_bissextile and int_day > 28))):
+        return False
+   
+    if (int_month % 2 == 0 and
+    ((int_month > 7 and int_day > 31)
+    or (int_month < 7 and int_day > 30))):
+        return False
+    elif (int_month % 2 == 1 and
+    ((int_month < 8 and int_day > 31) or
+    (int_month > 8 and int_day > 30))):
+        return False
+    
+    return True
+
+def is_a_number(number_to_test:str, variable_wanted: str) -> bool:
+    """Test if a string is a number"""
+    if not number_to_test.isdecimal():
+        print(f"{variable_wanted} must be a number")
+        return False
+    return True
