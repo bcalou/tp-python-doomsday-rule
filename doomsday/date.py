0570f0fccea241145d8be72b98aa1135eae1d6cf
JameFLC <jame.flochlecarour@gmail.com>
2022-09-28 20:34:22 +0200

    First pass of date checking working

--- doomsday/date.py
+++ doomsday/date.py
@@ -1,2 +1,43 @@
+
+
 def is_valid_date(date: str) -> bool:
-    return False
+    # Check input length
+    if not (len(date) <= 10 or len(date) >= 8):
+        return False
+
+    # Split date and check if the list contain 3 elements : YYYY/MM/DD
+    splited_date: list[str] = date.split("-")
+    if len(splited_date) != 3:
+        return False
+    # Check if the year input is a number that fits in the gregorian calendar
+    if not splited_date[0].isdigit() or int(splited_date[0]) < 1583:
+        return False
+
+    # Check if the month input is a number that is between 1 and 12
+    if not (splited_date[1].isdigit() and (int(splited_date[1]) <= 12 and int(splited_date[1]) >= 1)):
+        return False
+
+    return is_day_valid(splited_date[2], int(splited_date[1]), int(splited_date[0]))
+
+
+def is_day_valid(day_text: str, month: int, year: int) -> bool:
+
+    USUSAL_DAYS_PER_MONTH: list[int] = [
+        31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    # Check if day is in a valid format
+    if not day_text.isdigit():
+        return False
+
+    # Check for february and leap year case
+    day = int(day_text)
+
+    if is_leap_year(year) and month == 2 and (day <= 29 or day >= 1):
+        return True
+
+    # Check for casual cases
+    return day <= USUSAL_DAYS_PER_MONTH[month - 1] and day > 1
+
+
+def is_leap_year(year: int) -> bool:
+    return (year % 4 == 0 and not year % 100 == 0) or year % 400 == 0
--- test-date.py
+++ test-date.py
@@ -5,46 +5,46 @@ print("\n")
 if not is_valid_date('2021-10-10'):
     print('\033[91m❌ 2021-10-10 should be valid\n')
 
-if not is_valid_date('1996-02-29'):
+elif not is_valid_date('1996-02-29'):
     print('\033[91m❌ 1996-02-29 should be valid\n')
 
-if not is_valid_date('2000-02-29'):
+elif not is_valid_date('2000-02-29'):
     print('\033[91m❌ 2000-02-29 should be valid\n')
 
-if not is_valid_date('2148-01-31'):
+elif not is_valid_date('2148-01-31'):
     print('\033[91m❌ 2148-01-31 should be valid\n')
 
-if not is_valid_date('2148-1-2'):
+elif not is_valid_date('2148-1-2'):
     print('\033[91m❌ 2148-1-2 should be valid\n')
 
-if is_valid_date('2021-20-00'):
+elif is_valid_date('2021-20-00'):
     print('\033[91m❌ 2021-20-00 should not be valid\n')
 
-if is_valid_date('2021-20-02'):
+elif is_valid_date('2021-20-02'):
     print('\033[91m❌ 2021-20-02 should not be valid\n')
 
-if is_valid_date('1582-05-05'):
+elif is_valid_date('1582-05-05'):
     print('\033[91m❌ 1582-05-05 should not be valid\n')
 
-if is_valid_date('1900-02-29'):
+elif is_valid_date('1900-02-29'):
     print('\033[91m❌ 1900-02-29 should not be valid\n')
 
-if is_valid_date('2021-04-31'):
+elif is_valid_date('2021-04-31'):
     print('\033[91m❌ 2021-04-31 should not be valid\n')
 
-if is_valid_date('01-01-2020'):
+elif is_valid_date('01-01-2020'):
     print('\033[91m❌ 01-01-2020 should not be valid\n')
 
-if is_valid_date('01-01-'):
+elif is_valid_date('01-01-'):
     print('\033[91m❌ 01-01- should not be valid\n')
 
-if is_valid_date('-01-2020'):
+elif is_valid_date('-01-2020'):
     print('\033[91m❌ -01-2020 should not be valid\n')
 
-if is_valid_date('10-01-2020-02'):
+elif is_valid_date('10-01-2020-02'):
     print('\033[91m❌ 10-01-2020-02 should not be valid\n')
 
-if is_valid_date('10-01'):
+elif is_valid_date('10-01'):
     print('\033[91m❌ 10-01 should not be valid\n')
 
 else:
