45e6658384b1f234f74a65d3c3d7935a1586aa0f
Roucool_PvP <lucastag77@gmail.com>
2022-09-29 14:29:27 +0200

    Renommage et nettoyage

--- doomsday/algorithm.py
+++ doomsday/algorithm.py
@@ -10,14 +10,14 @@ def get_day_for_date(date: str) -> str:
     siecle: int = int(date[6:8])
     annee: int = int(date[8: ])
 
-    ancre_jour: int = get_anchor_day_of_the_week(get_year_anchor(siecle), annee)
+    anchor_weekday: int = get_anchor_weekday(get_year_anchor(siecle), annee)
     ancre_mois: int = get_month_anchor(mois, is_leap_year(annee))
 
-    jour_calcule: int = (jour - ancre_mois + ancre_jour)%7
+    jour_calcule: int = (jour - ancre_mois + anchor_weekday)%7
 
     return SEMAINE[jour_calcule]
 
-def get_anchor_day_of_the_week(year_anchor: int, year: int) -> int:
+def get_anchor_weekday(year_anchor: int, year: int) -> int:
     """Calculates the anchor day for the given year"""
     if year % 2 == 1 :
         year += 11
@@ -30,22 +30,21 @@ def get_anchor_day_of_the_week(year_anchor: int, year: int) -> int:
 
 def get_month_anchor(mois: int,is_leap_year: bool) -> int:
     """Return the month anchor for the given month"""
+    if mois <= 3:
+        return special_month_cases(mois, is_leap_year)
+    if mois == 5 or mois == 9:
+        return 14 - mois # On inverse mai et septembre
+    if mois == 7 or mois == 11:
+        return 18 - mois # On inverse juillet et novembre
+    return mois # Les autres mois correspondent à leur jour ancre
+
+def special_month_cases(mois: int, is_leap_year: bool) -> int:
+    """Completes get_month_anchor for special cases"""
     if mois == 1:
         return 10 if is_leap_year else 11
     if mois == 2:
         return 21 if is_leap_year else 22
-    if mois == 3:
-        return 0
-    if mois == 5:
-        return 9
-    if mois == 7:
-        return 11
-    if mois == 9:
-        return 5
-    if mois == 11:
-        return 7
-    return mois
-
+    return 0
 
 def is_leap_year(year: int) -> bool:
     """Return whether this year is a leap year"""
