1e1d4c2a006a45217cdfd65543a39d6077ab4da6
82L <16747176+82L@users.noreply.github.com>
2022-09-30 16:45:00 +0200

    algorithm functionnement

--- doomsday.py
+++ doomsday.py
@@ -1,4 +1,22 @@
 from doomsday.date import is_valid_date
-from doomsday.algorithm import get_day_for_date
+from doomsday.algorithm import get_weekday_for_date
+
 
 # Commencez ici !
+def get_date_and_return_day() -> None:
+    date: str = input("Give a date in format YYYY-MM-dd, we will give you its day")
+    day: str = find_day_of_date(date)
+    print(day)
+
+
+def find_day_of_date(date: str) -> str:
+    """Return day of date, if date is valid"""
+
+    day: str = "There is no day corresponding"
+    if is_valid_date(date):
+        day = get_weekday_for_date(date)
+
+    return day
+
+
+get_date_and_return_day()
