f8999c4d12c7e490b35e7930067b91afd1c41bdd
82L <16747176+82L@users.noreply.github.com>
2022-09-30 16:44:34 +0200

    update pep8

--- doomsday/algorithm.py
+++ doomsday/algorithm.py
@@ -1,22 +1,18 @@
 DAYS_TABLE: list[str] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-MONTH_KEY_DAYS_POSITION: list[int] = [0,0,0,4,9,6,11,8,5,10,7,12]
-CENTURY_CORRECTOR_EQUIVALENCE: list[int] = [2,0,5,3]
+MONTH_KEY_DAYS_POSITION: list[int] = [0, 0, 0, 4, 9, 6, 11, 8, 5, 10, 7, 12]
 
-from calendar import weekday
-from operator import is_
-from doomsday.date import is_year_bissextile
+CENTURY_CORRECTOR_EQUIVALENCE: list[int] = [2, 0, 5, 3]
 
+from doomsday.date import find_if_is_bissextile
 
-def get_day_for_date(date: str) -> str:
-    return "Monday"
 
 def get_weekday_for_date(date: str) -> str:
     """Return the week day of a date"""
-    
+
     date_as_list: list[str] = date.split("-")
-    key_day: int = get_key_day(year = date_as_list[0])
-    month_key_day_position: int = get_position_key_day_in_month(month = date_as_list[1], year = date_as_list[0])
+    key_day: int = get_key_day(year=date_as_list[0])
+    month_key_day_position: int = get_position_key_day_in_month(month=date_as_list[1], year=date_as_list[0])
 
     position_difference_with_key_day: int = (int(date_as_list[2]) - month_key_day_position) % 7
     weekday: int = (key_day + position_difference_with_key_day) % 7
@@ -24,15 +20,16 @@ def get_weekday_for_date(date: str) -> str:
     return DAYS_TABLE[weekday]
 
 
-def get_century_balise(century: str) -> int :
+def get_century_balise(century: str) -> int:
     """Find the century balise, based on century % 4"""
 
     int_century: int = int(century)
     type: int = int_century % 4
     return CENTURY_CORRECTOR_EQUIVALENCE[type]
-    
+
+
 def get_operations_on_end_year(years: str) -> int:
-    """Do the preliminary operations on the 2 last digits of year"""
+    """Do the preliminary operations on the last 2  digits of year"""
     int_years: int = int(years)
 
     int_years = add_11_if_number_is_odd(int_years)
@@ -40,18 +37,22 @@ def get_operations_on_end_year(years: str) -> int:
     int_years = add_11_if_number_is_odd(int_years)
 
     superior_multiple_of_7: int = get_superior_multiple_of_seven(int_years)
-    
+
     return (superior_multiple_of_7 * 7) - int_years
 
+
 def add_11_if_number_is_odd(number: int) -> int:
+    """Add eleven if the number given is odd"""
 
     if number % 2 != 0:
         number += 11
     return number
 
-def get_superior_multiple_of_seven(number: int) -> int:
 
-    superior_multiple_of_7: int =  (number // 7)
+def get_superior_multiple_of_seven(number: int) -> int:
+    """Get the next multiple of 7 above or equal to given number"""
+
+    superior_multiple_of_7: int = (number // 7)
     if superior_multiple_of_7 != 0:
         superior_multiple_of_7 += 1
 
@@ -67,19 +68,17 @@ def get_key_day(year: str) -> int:
     century_balise: int = get_century_balise(year[:year_length - 2])
 
     return (transformed_year + century_balise) % 7
-    
-def get_position_key_day_in_month( month: str, year: str) -> int :
+
+
+def get_position_key_day_in_month(month: str, year: str) -> int:
     """Get position of key day in given month"""
 
-    int_month:int = int(month)
+    int_month: int = int(month)
 
-    if int_month < 3 :
-        if is_year_bissextile(int(year)) :
+    if int_month < 3:
+        if find_if_is_bissextile(int(year)):
             return 11 if (int_month == 1) else 22
-        else :
+        else:
             return 10 if (int_month == 1) else 21
     else:
         return MONTH_KEY_DAYS_POSITION[int_month - 1]
-           
-        
-
--- doomsday/date.py
+++ doomsday/date.py
@@ -1,34 +1,43 @@
 GREGORIAN_CALENDAR_START: int = 1583
 FEBRUARY_NUMBER: int = 2
-MAX_DATE_MONTH: int = 31
 MIN_LENGTH_DATE_STRING: int = 8
 DATE_ELEMENT_NUMBER: int = 3
+
 
 def is_valid_date(date: str) -> bool:
     """Look if the date does not violate the terms for conway algorithm"""
 
-    if len(date) < MIN_LENGTH_DATE_STRING :
-        print("Date is not long enough to be valid")
-        return False
-    
-    date_as_list: list[str] = date.split("-")
+    date_as_list: list[str] = check_date_string_and_split_it(date)
 
-    if len(date_as_list) != DATE_ELEMENT_NUMBER:
-        print("Too much or too less date elements")
+    if not date_as_list:
         return False
-    
+
     year: str = date_as_list[0]
     month: str = date_as_list[1]
     day: str = date_as_list[2]
 
-    if not is_valid_year(year):
+    if (not is_valid_year(year) and
+            not is_valid_month_and_day(month, day, find_if_is_bissextile(int(year)))):
         return False
-    
-    if not is_valid_month_and_day(month, day, is_year_bissextile(int(year))):
-        return False
-    
+
     return True
-    
+
+
+def check_date_string_and_split_it(date: str) -> list[str]:
+    """Check date string and make it a list"""
+
+    if len(date) < MIN_LENGTH_DATE_STRING:
+        print("Date is not long enough to be valid")
+        return []
+
+    date_as_list: list[str] = date.split("-")
+
+    if len(date_as_list) != DATE_ELEMENT_NUMBER:
+        print("Too much or too less date elements")
+        return []
+
+    return date_as_list
+
 
 def is_valid_year(year: str) -> bool:
     """Check if year is a number above start of gregorian calendar"""
@@ -36,26 +45,28 @@ def is_valid_year(year: str) -> bool:
     if not year.isdecimal():
         print("Year must be a number")
         return False
-    
-    if int(year) < GREGORIAN_CALENDAR_START: 
+
+    if int(year) < GREGORIAN_CALENDAR_START:
         print("Year must be after 1582")
         return False
-    
+
     return True
 
 
-def is_year_bissextile(year: int) -> bool:
+def find_if_is_bissextile(year: int) -> bool:
     """Check if a year is multiple of four, and not multiple of 100 but multiple of 400"""
-    return (year % 4 == 0 and ( year % 100 != 0 or year % 400 == 0))
+
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
 
 def is_valid_month_and_day(month: str, day: str, is_year_bissextile: bool) -> bool:
-    
+    """Check if month and day are coherent"""
+
     if not (is_a_number(month, "Month") and is_a_number(day, "Day")):
         return False
-    
-    int_day:int = int(day)
-    int_month:int = int(month)
+
+    int_day: int = int(day)
+    int_month: int = int(month)
 
     if 1 > int_month or int_month > 12:
         print("Month must be between 1 and 12")
@@ -66,23 +77,26 @@ def is_valid_month_and_day(month: str, day: str, is_year_bissextile: bool) -> bo
         return False
 
     if (int_month == FEBRUARY_NUMBER and
-        ((is_year_bissextile and int_day > 29) or 
-        (not is_year_bissextile and int_day > 28))):
+            ((is_year_bissextile and int_day > 29) or
+             (not is_year_bissextile and int_day > 28))):
         return False
-   
+
     if (int_month % 2 == 0 and
-    ((int_month > 7 and int_day > 31)
-    or (int_month < 7 and int_day > 30))):
+            ((int_month > 7 and int_day > 31)
+             or (int_month < 7 and int_day > 30))):
         return False
+
     elif (int_month % 2 == 1 and
-    ((int_month < 8 and int_day > 31) or
-    (int_month > 8 and int_day > 30))):
+          ((int_month < 8 and int_day > 31) or
+           (int_month > 8 and int_day > 30))):
         return False
-    
+
     return True
 
-def is_a_number(number_to_test:str, variable_wanted: str) -> bool:
+
+def is_a_number(number_to_test: str, variable_wanted: str) -> bool:
     """Test if a string is a number"""
+
     if not number_to_test.isdecimal():
         print(f"{variable_wanted} must be a number")
         return False
