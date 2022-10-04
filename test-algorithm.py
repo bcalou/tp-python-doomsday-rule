70956dd7ef841abd6b9f77aea9865ec3910ef460
bcalou <bastien.calou@gmail.com>
2022-10-04 14:44:57 +0200

    improve test algorithms

--- test-algorithm.py
+++ test-algorithm.py
@@ -1,61 +1,81 @@
 from doomsday.algorithm import get_weekday_for_date
 
+error: bool = False
+
 if get_weekday_for_date("2021-01-31") != "Sunday":
     print('\033[91m❌ Date for 2021-01-31 should be Sunday')
 
-elif get_weekday_for_date("2021-02-20") != "Saturday":
+if get_weekday_for_date("2021-02-20") != "Saturday":
+    error = True
     print('\033[91m❌ Date for 2021-02-20 should be Saturday')
 
-elif get_weekday_for_date("2021-03-10") != "Wednesday":
+if get_weekday_for_date("2021-03-10") != "Wednesday":
+    error = True
     print('\033[91m❌ Date for 2021-03-10 should be Wednesday')
 
-elif get_weekday_for_date("2021-04-10") != "Saturday":
+if get_weekday_for_date("2021-04-10") != "Saturday":
+    error = True
     print('\033[91m❌ Date for 2021-04-10 should be Saturday')
 
-elif get_weekday_for_date("2021-05-10") != "Monday":
+if get_weekday_for_date("2021-05-10") != "Monday":
+    error = True
     print('\033[91m❌ Date for 2021-05-10 should be Monday')
 
-elif get_weekday_for_date("2021-06-10") != "Thursday":
+if get_weekday_for_date("2021-06-10") != "Thursday":
+    error = True
     print('\033[91m❌ Date for 2021-06-10 should be Thursday')
 
-elif get_weekday_for_date("2021-07-10") != "Saturday":
+if get_weekday_for_date("2021-07-10") != "Saturday":
+    error = True
     print('\033[91m❌ Date for 2021-07-10 should be Saturday')
 
-elif get_weekday_for_date("2021-08-10") != "Tuesday":
+if get_weekday_for_date("2021-08-10") != "Tuesday":
+    error = True
     print('\033[91m❌ Date for 2021-08-10 should be Tuesday')
 
-elif get_weekday_for_date("2021-09-10") != "Friday":
+if get_weekday_for_date("2021-09-10") != "Friday":
+    error = True
     print('\033[91m❌ Date for 2021-09-10 should be Friday')
 
-elif get_weekday_for_date("2021-10-10") != "Sunday":
+if get_weekday_for_date("2021-10-10") != "Sunday":
+    error = True
     print('\033[91m❌ Date for 2021-10-10 should be Sunday')
 
-elif get_weekday_for_date("2021-11-10") != "Wednesday":
+if get_weekday_for_date("2021-11-10") != "Wednesday":
+    error = True
     print('\033[91m❌ Date for 2021-11-10 should be Wednesday')
 
-elif get_weekday_for_date("2021-12-10") != "Friday":
+if get_weekday_for_date("2021-12-10") != "Friday":
+    error = True
     print('\033[91m❌ Date for 2021-12-10 should be Friday')
 
-elif get_weekday_for_date("2000-01-10") != "Monday":
+if get_weekday_for_date("2000-01-10") != "Monday":
+    error = True
     print('\033[91m❌ Date for 2000-01-10 should be Monday')
 
-elif get_weekday_for_date("1900-01-10") != "Wednesday":
+if get_weekday_for_date("1900-01-10") != "Wednesday":
+    error = True
     print('\033[91m❌ Date for 1900-01-10 should be Wednesday')
 
-elif get_weekday_for_date("2000-02-29") != "Tuesday":
+if get_weekday_for_date("2000-02-29") != "Tuesday":
+    error = True
     print('\033[91m❌ Date for 2000-02-29 should be Tuesday')
 
-elif get_weekday_for_date("1900-02-28") != "Wednesday":
+if get_weekday_for_date("1900-02-28") != "Wednesday":
+    error = True
     print('\033[91m❌ Date for 1900-02-28 should be Wednesday')
 
-elif get_weekday_for_date("2159-01-10") != "Wednesday":
+if get_weekday_for_date("2159-01-10") != "Wednesday":
+    error = True
     print('\033[91m❌ Date for 2159-01-10 should be Wednesday')
 
-elif get_weekday_for_date("2259-01-10") != "Monday":
+if get_weekday_for_date("2259-01-10") != "Monday":
+    error = True
     print('\033[91m❌ Date for 2259-01-10 should be Monday')
 
-elif get_weekday_for_date("2359-01-10") != "Saturday":
+if get_weekday_for_date("2359-01-10") != "Saturday":
+    error = True
     print('\033[91m❌ Date for 2359-01-10 should be Saturday')
 
-else:
+if not error:
     print('\033[92m✓ OK')
--- test-date.py
+++ test-date.py
@@ -1,49 +1,66 @@
 from doomsday.date import is_valid_date
 
+error: bool = False
+
 if not is_valid_date('2021-10-10'):
+    error = True
     print('\033[91m❌ 2021-10-10 should be valid')
 
-elif not is_valid_date('1996-02-29'):
+if not is_valid_date('1996-02-29'):
+    error = True
     print('\033[91m❌ 1996-02-29 should be valid')
 
-elif not is_valid_date('2000-02-29'):
+if not is_valid_date('2000-02-29'):
+    error = True
     print('\033[91m❌ 2000-02-29 should be valid')
 
-elif not is_valid_date('2148-01-31'):
+if not is_valid_date('2148-01-31'):
+    error = True
     print('\033[91m❌ 2148-01-31 should be valid')
 
-elif not is_valid_date('2148-1-2'):
+if not is_valid_date('2148-1-2'):
+    error = True
     print('\033[91m❌ 2148-1-2 should be valid')
 
-elif is_valid_date('2021-20-00'):
+if is_valid_date('2021-20-00'):
+    error = True
     print('\033[91m❌ 2021-20-00 should not be valid')
 
-elif is_valid_date('2021-20-02'):
+if is_valid_date('2021-20-02'):
+    error = True
     print('\033[91m❌ 2021-20-02 should not be valid')
 
-elif is_valid_date('1582-05-05'):
+if is_valid_date('1582-05-05'):
+    error = True
     print('\033[91m❌ 1582-05-05 should not be valid')
 
-elif is_valid_date('1900-02-29'):
+if is_valid_date('1900-02-29'):
+    error = True
     print('\033[91m❌ 1900-02-29 should not be valid')
 
-elif is_valid_date('2021-04-31'):
+if is_valid_date('2021-04-31'):
+    error = True
     print('\033[91m❌ 2021-04-31 should not be valid')
 
-elif is_valid_date('01-01-2020'):
+if is_valid_date('01-01-2020'):
+    error = True
     print('\033[91m❌ 01-01-2020 should not be valid')
 
-elif is_valid_date('01-01-'):
+if is_valid_date('01-01-'):
+    error = True
     print('\033[91m❌ 01-01- should not be valid')
 
-elif is_valid_date('-01-2020'):
+if is_valid_date('-01-2020'):
+    error = True
     print('\033[91m❌ -01-2020 should not be valid')
 
-elif is_valid_date('10-01-2020-02'):
+if is_valid_date('10-01-2020-02'):
+    error = True
     print('\033[91m❌ 10-01-2020-02 should not be valid')
 
-elif is_valid_date('10-01'):
+if is_valid_date('10-01'):
+    error = True
     print('\033[91m❌ 10-01 should not be valid')
 
-else:
+if not error:
     print('\033[92m✓ OK')
