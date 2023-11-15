0a8004dcd74baecd100a56b2b4d1edf48280390e
Loïs L. <36632834+LifestreamVII@users.noreply.github.com>
2023-11-15 15:36:48 +0100

    Main file

--- __main__.py
+++ __main__.py
@@ -1,5 +1,21 @@
-def main() -> None:
-    print("Hello world")
+import datetime
 
+def input_date() -> None:
+    """Asks for a date and check if its valid"""
+    time_str = str(input("Enter date in this format yyyy-mm-dd \n >>"))
+    try:
+        # year = time_str[0:4]
+        # month = time_str[5:7]
+        # day = time_str[8:10]
+        date = time_str.split("-", 3)
+        year = int(date[0])
+        month = int(date[1])
+        day = int(date[2])
+        if (year > 999 and 1 <= month <= 12 and 1 <= day <= 31):
+            print("Valid date")
+        else: 
+            raise Exception("Invalid date")
+    except Exception as e:
+        print(e)
 
-main()
+input_date()
