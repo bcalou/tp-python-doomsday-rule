b7113dee628a013aaeddf869fe922f8f25822cf1
DESKTOP-ISVPJFT\Mathieu <mat.chico86.mc@gmail.com>
2023-11-15 21:17:27 +0100

    commit Mathieu Chicoine

--- doomsday/algorithm.py
+++ doomsday/algorithm.py
@@ -1,7 +1,8 @@
 def get_weekday_for_date(date: str) -> str:
-    """à venir"""
-
-    print(date)
+    """
+    Détermine le jour du jugement dernier à partir d'une date donnée
+    Format attendu : YYYY-MM-dd
+    """
 
     days_of_week = (
         'Sunday',
@@ -21,40 +22,16 @@ def get_weekday_for_date(date: str) -> str:
 
     # 1re partie : déterminer le jour "ancre" de l'année
 
-    year_end: float = float(date_in_list[0][2:]) # La fin de l'année
-    century: int = int(date_in_list[0][:2]) # Le siècle
-    anchor_day: int = 0 # Le jour ancre
-    century_value: int = 0 # Le décalage lié au siècle
-
-    if year_end % 2 != 0:
-        year_end += 11
-    year_end /= 2
-    if year_end % 2 != 0:
-        year_end += 11
-
-    multiple_of_7: int = int(year_end)
-    while multiple_of_7 % 7 != 0: # Obtenir le multiple de 7 supérieur ou égal
-        multiple_of_7 += 1
-
-    match (century % 4): # Obtenir le décalage du siècle
-        case 0: # Dates 1600, 2000, etc.
-            century_value = 2
-        case 1: # Dates 1700, 2100, etc.
-            century_value = 0
-        case 2: # Dates 1800, 2200, etc.
-            century_value = 5
-        case 3: # Dates 1500, 1900, etc.
-            century_value = 3
-
-
-    anchor_day += multiple_of_7 - int(year_end) + century_value
+    anchor_day: int = get_anchor_day(year)
 
     # 2e partie : déterminer un jour particulier en fonction du jour ancre
 
-    if is_leap_year(year) and month == 1:
-        return days_of_week[(day - 4 + anchor_day) % 7]
-    if is_leap_year(year) and month == 2:
-        return days_of_week[(day - 1 + anchor_day) % 7]
+    if is_leap_year(year):
+        match month:
+            case 1:
+                return days_of_week[(day - 4 + anchor_day) % 7]
+            case 2:
+                return days_of_week[(day - 1 + anchor_day) % 7]
 
     match month:
         case 1:
@@ -88,3 +65,35 @@ def is_leap_year(year: int) -> bool:
     """Renvoie 'vrai' si l'année en paramètre est bissextile"""
 
     return True if (year%400 == 0) or (year%4 == 0 and year%100 !=0) else False
+
+def get_anchor_day(year: int) -> int:
+    """Détermine le jour 'ancre' d'une année"""
+
+    year_end: float = year % 100 # La fin de l'année
+    century: int = int(year / 100) # Le siècle
+    century_value: int = 0 # Le décalage lié au siècle
+
+    # Si fin d'année pair /2, sinon +11 et /2 puis si toujours impair +11
+    if year_end % 2 != 0:
+        year_end += 11
+    year_end /= 2
+    if year_end % 2 != 0:
+        year_end += 11
+
+    # Obtenir le multiple de 7 supérieur ou égal
+    multiple_of_7: int = int(year_end)
+    while multiple_of_7 % 7 != 0:
+        multiple_of_7 += 1
+
+    # Obtenir le décalage du siècle
+    match (century % 4):
+        case 0: # Dates 1600, 2000, etc.
+            century_value = 2
+        case 1: # Dates 1700, 2100, etc.
+            century_value = 0
+        case 2: # Dates 1800, 2200, etc.
+            century_value = 5
+        case 3: # Dates 1500, 1900, etc.
+            century_value = 3
+
+    return multiple_of_7 - int(year_end) + century_value
