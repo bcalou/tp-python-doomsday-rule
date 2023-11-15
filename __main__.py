a4a49c6abfa1a285d3929dea716c6d8150279e90
bterral <bterral@etudiant.univ-lr.fr>
2023-11-15 15:17:12 +0100

    First commit after fork

--- __main__.py
+++ __main__.py
@@ -1,5 +1,13 @@
-def main() -> None:
-    print("Hello world")
+from doomsday.algorithm import get_weekday_for_date
 
+
+def main() -> None:
+    dates = ["2000-01-01",
+             "2001-01-29",
+             "2017-12-15",
+             "2023-11-15",
+             "2111-12-12"] # Saturday, Monday, Friday, Wednesday, Saturday
+    for date in dates:
+        print("{0} is a {1}.".format(date, get_weekday_for_date(date)))
 
 main()
--- doomsday/date.py
+++ doomsday/date.py
@@ -1,2 +1,18 @@
 def is_valid_date(date: str) -> bool:
-    return True
+    try:
+        year, month, day = map(int, date.split('-'))
+
+        if 1 <= month <= 12 and 1 <= day <= 31:
+            if month in [4, 6, 9, 11] and day > 30:
+                return False  
+            elif month == 2: # February
+                if day > 29:    
+                    return False 
+                elif day == 29 and not (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)):
+                    return False  # February 29 is valid only in leap years
+
+            return True
+    except ValueError:
+        pass  
+
+    return False  
