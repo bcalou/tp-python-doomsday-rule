7a3ee2daeb60bfb67b6351a76b42a224d43a60a0
Timo <timetais22@gmail.com>
2023-11-15 12:27:30 +0100

    date input

--- __main__.py
+++ __main__.py
@@ -1,5 +1,13 @@
+from doomsday.date import is_valid_date
+
+
 def main() -> None:
-    print("Hello world")
+    user_input = input("Please enter a date in the format YYYY-MM-dd: ")
+
+    if is_valid_date(user_input):
+        print("The date is valid.")
+    else:
+        print("Error: The date is not valid.")
 
 
 main()
--- doomsday/date.py
+++ doomsday/date.py
@@ -1,2 +1,24 @@
-def is_valid_date(date: str) -> bool:
-    return True
+from datetime import datetime
+
+
+def is_valid_date(date_str) -> bool:
+    try:
+        # Try to convert the string to a date object
+        date_obj = datetime.strptime(date_str, '%Y-%m-%d')
+
+        # Check if the year is greater than or equal to 1583
+        if date_obj.year < 1583:
+            print("Error: The year must be greater than or equal to 1583.")
+            return False
+
+        return True
+
+    except ValueError:
+        # Conversion error, the date is not in the expected format
+        print("Error: The date does not follow the format YYYY-MM-dd.")
+        return False
+
+    except Exception as e:
+        # Handle other potential errors
+        print(f"An error occurred: {e}")
+        return False
