3f5c6a9877cc8e91f93f5596f71f5c81d0ca7b02
MZASU <theo.thenault@gmail.com>
2023-11-15 13:28:02 +0100

    Squelette validation de la date

--- __main__.py
+++ __main__.py
@@ -1,5 +1,14 @@
+from doomsday.date import *
+
+
 def main() -> None:
-    print("Hello world")
+    date = input("Veuillez saisir une date au format YYYY-MM-DD: ")
+
+    if is_valid_date(date):
+        print("Date valide!")
+        # Exécuter l'algorithme
+    else:
+        print("Erreur")
 
 
 main()
--- doomsday/date.py
+++ doomsday/date.py
@@ -1,2 +1,26 @@
 def is_valid_date(date: str) -> bool:
+    """Verify if the date parameter is valid
+
+    First the function verify the format,
+    then it verify if the date exist
+    """
+
+    if not is_format_valid(date):
+        print("Le format de la date n'est pas YYYY-MM-DD!")
+        return False
+
+    if not does_date_exist(date):
+        print("La date n'existe pas!")
+        return False
+
+    return True
+
+
+def is_format_valid(date: str) -> bool:
+    """Verify if date follows the format YYYY-MM-DD"""
+    return True
+
+
+def does_date_exist(date: str) -> bool:
+    """Verify if the date exist"""
     return True
