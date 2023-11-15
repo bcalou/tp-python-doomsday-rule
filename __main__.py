c3acb5be6ae9caae6b1afbe2d6d02d4796e2f330
Loïs L. <36632834+LifestreamVII@users.noreply.github.com>
2023-11-15 16:02:02 +0100

    Context on errors

--- __main__.py
+++ __main__.py
@@ -1,5 +1,3 @@
-import datetime
-
 def input_date() -> None:
     """Asks for a date and check if its valid"""
     time_str = str(input("Enter date in this format yyyy-mm-dd \n >>"))
@@ -8,14 +6,17 @@ def input_date() -> None:
         # month = time_str[5:7]
         # day = time_str[8:10]
         date = time_str.split("-", 3)
-        year = int(date[0])
-        month = int(date[1])
-        day = int(date[2])
-        if (year > 999 and 1 <= month <= 12 and 1 <= day <= 31):
-            print("Valid date")
-        else: 
-            raise Exception("Invalid date")
-    except Exception as e:
+        year = int(date[0]) if int(date[0]) >= 1583 else None
+        month = int(date[1]) if 1 <= int(date[1]) <= 12 else None
+        day = int(date[2]) if 1 <= int(date[2]) <= 31 else None
+        if year is None:
+            raise ValueError("Invalid year")
+        if month is None:
+            raise ValueError("Invalid month")
+        if day is None:
+            raise ValueError("Invalid day")
+        print("Valid date")
+    except ValueError as e:
         print(e)
 
 input_date()
