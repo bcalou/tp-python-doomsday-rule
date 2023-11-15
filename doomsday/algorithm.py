e3cc654034b2112bec83e7fcc4c40b1103027767
The17thDoctor <40616434+The17thDoctor@users.noreply.github.com>
2023-11-15 16:40:44 +0100

    Fix commentaires algorithm.py

--- doomsday/algorithm.py
+++ doomsday/algorithm.py
@@ -13,6 +13,27 @@ DAY_NAMES = (
 )
 
 
+def get_weekday_for_date(date: str) -> str:
+    """Returns the name of the day at a given date."""
+    date_split: list[str] = date.split("-")
+    year: int = int(date_split[0])
+    month: int = int(date_split[1])
+    day: int = int(date_split[2])
+
+    year_anchor_day: int = get_anchor_day_for_year(year)
+    month_anchor: int = get_anchor_for_month(month, year)
+
+    # Calculate the number of days between the month's anchor day and
+    # the wanted day.
+    day_offset: int = abs(day - month_anchor)
+    # Since the month's anchor day and the year's anchor day are the same
+    # we can add the year's anchor day index to get the final day index.
+    final_day_index = (year_anchor_day + day_offset) % 7
+
+    return DAY_NAMES[final_day_index]
+
+
+
 def get_anchor_day_for_year(year: int) -> int:
     """Returns the anchor day of a given year starting at 0 for Sunday"""
 
@@ -36,37 +57,10 @@ def get_anchor_day_for_year(year: int) -> int:
 def get_anchor_for_month(month: int, year: int) -> int:
     """Returns the anchor day of a given month given the year"""
 
-    match (month):
-        case 4, 6, 8, 10, 12:
-            return month
-        case 1:
-            return 11 if date.is_leap_year(year) else 10
-        case 2:
-            return 22 if date.is_leap_year(year) else 21
-        case 3:
-            return 0
-        case 5:
-            return 9
-        case 7:
-            return 11
-        case 9:
-            return 5
-        case 11:
-            return 7
+    month_anchors = (
+        11 if date.is_leap_year(year) else 10,
+        22 if date.is_leap_year(year) else 21,
+        0, 4, 9, 6, 11, 8, 7, 10, 7, 12
+    )
 
-    return 0
-
-
-def get_weekday_for_date(date: str) -> str:
-    date_split: list[str] = date.split("-")
-    year: int = int(date_split[0])
-    month: int = int(date_split[1])
-    day: int = int(date_split[2])
-
-    year_anchor_day: int = get_anchor_day_for_year(year)
-    month_anchor: int = get_anchor_for_month(month, year)
-    day_offset: int = abs(day - month_anchor)
-
-    final_day_index = (year_anchor_day + day_offset) % 7
-
-    return DAY_NAMES[final_day_index]
+    return month_anchors[month - 1]
