a653eebe77f3d418be62004f5967865a67830c57
joffrey3 <fouche.joffrey@gmail.com>
2023-11-17 09:33:13 +0100

    fin

--- __main__.py
+++ __main__.py
@@ -7,10 +7,11 @@ def main() -> None:
     when the date is correct,
     main function ask for the day of the date and return it to the user"""
 
-    print("Please enter a date if you want to know it day on week")
-    input_date: str = input()
-    while (not date.is_valid_date(input_date)):
-        input_date = input()
+    while True:
+        input_date: str = input(
+            "Please enter a date if you want to know it day on week")
+        if (date.is_valid_date(input_date)):
+            break
     print(input_date + " is a " + algorithm.get_weekday_for_date(input_date))
 
 
