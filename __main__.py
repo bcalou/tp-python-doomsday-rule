7a9bf33de8d7631a3ecea65e56895a1fa4b555e5
MZASU <theo.thenault@gmail.com>
2023-11-17 10:43:20 +0100

    Boucle si la date saisit est invalide

--- __main__.py
+++ __main__.py
@@ -3,12 +3,13 @@ from doomsday.algorithm import *
 
 
 def main() -> None:
-    date = input("Veuillez saisir une date au format YYYY-MM-DD: ")
 
-    if is_valid_date(date):
-        print(f"Le {date} est un {get_weekday_for_date(date)}.")
-    else:
-        print("Erreur")
+    while True:
+        date = input("Veuillez saisir une date au format YYYY-MM-DD: ")
+
+        if is_valid_date(date):
+            print(f"Le {date} est un {get_weekday_for_date(date)}.")
+            break
 
 
 main()
