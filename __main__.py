37e3624f82ece9fc0017567ad14266f1b85d44f7
Loïs L <36632834+LifestreamVII@users.noreply.github.com>
2023-11-20 01:47:23 +0100

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -1,22 +1,13 @@
+from doomsday.date import is_valid_date
+from doomsday.algorithm import get_weekday_for_date
+
 def input_date() -> None:
     """Asks for a date and check if its valid"""
     time_str = str(input("Enter date in this format yyyy-mm-dd \n >>"))
-    try:
-        # year = time_str[0:4]
-        # month = time_str[5:7]
-        # day = time_str[8:10]
-        date = time_str.split("-", 3)
-        year = int(date[0]) if int(date[0]) >= 1583 else None
-        month = int(date[1]) if 1 <= int(date[1]) <= 12 else None
-        day = int(date[2]) if 1 <= int(date[2]) <= 31 else None
-        if year is None:
-            raise ValueError("Invalid year")
-        if month is None:
-            raise ValueError("Invalid month")
-        if day is None:
-            raise ValueError("Invalid day")
-        print("Valid date")
-    except ValueError as e:
-        print(e)
+    if is_valid_date(time_str):
+        result = get_weekday_for_date(time_str)
+        print(f"The day of the week is: {result}")
+    else:
+        return False
 
 input_date()
--- doomsday/date.py
+++ doomsday/date.py
@@ -1,2 +1,30 @@
-def is_valid_date(date: str) -> bool:
-    return True
+def is_valid_date(date: str):
+    try:
+        if not date:
+            raise ValueError("Empty date string")
+
+        date = date.split("-", 3)
+        if len(date) != 3:
+            raise ValueError("Invalid date format")
+
+        year = int(date[0]) if date[0] and int(date[0]) >= 1583 else None
+        month = int(date[1]) if date[1] and 1 <= int(date[1]) <= 12 else None
+        if year is None:
+            raise ValueError("Invalid year")
+        if month is None:
+            raise ValueError("Invalid month")
+        if date[2] and month in [1, 3, 5, 7, 8, 10, 12]:
+            day = int(date[2]) if 1 <= int(date[2]) <= 31 else None
+        elif date[2] and month in [4, 6, 9, 11]:
+            day = int(date[2]) if 1 <= int(date[2]) <= 30 else None
+        elif date[2] and month == 2:
+            day = int(date[2]) if is_leap_year(year) and int(date[2]) > 0 and int(date[2]) < 30 or not is_leap_year(year) and int(date[2]) > 0 and int(date[2]) < 29 else None
+        if day is None:
+            raise ValueError("Invalid day")
+        return True
+
+    except ValueError as e:
+        print(e)
+
+def is_leap_year(year: int) -> bool:
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
